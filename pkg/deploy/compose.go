9525b027f9944a8775ccb124334911e0b6fc4e33
Kingram <kingram@163.com>
2022-09-23 17:42:08 +0800

    fix: restart docker-compose
    Signed-off-by: Kingram <kingram@163.com>

--- pkg/deploy/compose.go
+++ pkg/deploy/compose.go
@@ -16,19 +16,19 @@ func Start(file, name string) error {
 	basefile := filepath.Base(file)
 	pname := basefile[7:strings.LastIndex(basefile, ".yaml")]
 	pname = "ad-" + name + "-" + pname
-	cmd := fmt.Sprintf("docker-compose -f %s -p %s up -d --remove-orphans", file, pname)
+	cmd := fmt.Sprintf("docker compose -f %s -p %s up -d --remove-orphans", file, pname)
 	return exec(cmd)
 }
 
 func exec(cmd string) error {
-	fmt.Printf("# exec cmd: %s \n", cmd)
+	// fmt.Printf("# exec cmd: %s \n", cmd)
 	_, err := script.Exec(cmd).Stdout()
 	return err
 }
 
 // stop docker-compose
 func Stop(file string) error {
-	cmd := fmt.Sprintf("docker-compose -f %s stop -t 20", file)
+	cmd := fmt.Sprintf("docker compose -f %s stop -t 20", file)
 	return exec(cmd)
 }
 
@@ -37,7 +37,7 @@ func Down(file, name string) error {
 	basefile := filepath.Base(file)
 	pname := basefile[7:strings.LastIndex(basefile, ".yaml")]
 	pname = "ad-" + name + "-" + pname
-	cmd := fmt.Sprintf("docker-compose -f %s -p %s down --remove-orphans", file, pname)
+	cmd := fmt.Sprintf("docker compose -f %s -p %s down --remove-orphans", file, pname)
 	return exec(cmd)
 }
 
@@ -45,8 +45,16 @@ func Restart(file, name string) error {
 	basefile := filepath.Base(file)
 	pname := basefile[7:strings.LastIndex(basefile, ".yaml")]
 	pname = "ad-" + name + "-" + pname
-	cmd := fmt.Sprintf("docker-compose -f %s -p %s up -d --remove-orphans && docker-compose -f %s -p %s restart", file, pname, file, pname)
-	return exec(cmd)
+	cmd1 := fmt.Sprintf("docker compose -f %s -p %s up -d --remove-orphans", file, pname)
+
+	cmd2 := fmt.Sprintf("docker compose -f %s -p %s restart", file, pname)
+
+	err := exec(cmd1)
+	if err != nil {
+		return err
+	}
+
+	return exec(cmd2)
 }
 
 func CheckReleaseDeploy(name string) (int, error) {
